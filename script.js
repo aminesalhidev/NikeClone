8c06529a6a876ae221e09058c974519e12e5b036
Amine Salhi <142630269+aminesalhidev@users.noreply.github.com>
2023-09-10 22:03:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function scrollProducts() {
     const container = document.querySelector('.products-container');
-    container.scrollBy(400, 0); // Scorrimento di 400 pixel a destra (puoi regolare questo valore come preferisci)
+    container.scrollBy(400, 0);
   }
   
